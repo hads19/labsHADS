33eb34f89093797f66bfd0f98a04492e05b29206
SWG10CI <swg10ci@gmail.com>
2018-02-10 20:12:43 +0100

    Iniciov2

---
+++ labsHADS/labsHADS/Inicio.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Inicio.aspx.cs" Inherits="labsHADS.Inicio" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Inicio</title>
+       <style type="text/css">
+        #form1 {
+            z-index: 1;
+            left: 10px;
+            top: 15px;
+            position: absolute;
+            height: 300px;
+            width: 600px;
+        }
+        #Inicio {
+            z-index: 1;
+            left: 10px;
+            top: 15px;
+            position: absolute;
+            height: 240px;
+            width: 617px;
+        }
+    </style>
+</head>
+<body>
+    <form id="Inicio" runat="server">
+        <div style="height: 240px">
+
+           <asp:Table ID="datos" runat="server" style="z-index: 1; left: 50px; top: 50px; position: absolute; height: 54px; width: 324px">
+                <asp:TableRow>
+                    <asp:TableCell>Email</asp:TableCell>
+                    <asp:TableCell><asp:TextBox ID="email" runat="server"></asp:TextBox></asp:TableCell>
+                    <asp:TableCell><asp:RequiredFieldValidator ID="erroremail" runat="server" ErrorMessage="*" ControlToValidate="email"></asp:RequiredFieldValidator></asp:TableCell>
+        
+                </asp:TableRow>
+               <asp:TableRow>
+                   <asp:TableCell>Password</asp:TableCell><asp:TableCell><asp:TextBox runat="server" ID="password" TextMode="Password"/></asp:TableCell>
+                   <asp:TableCell><asp:RequiredFieldValidator ID="errorpass" runat="server" ErrorMessage="*" ControlToValidate="password"></asp:RequiredFieldValidator></asp:TableCell>
+               </asp:TableRow></asp:Table><asp:Table ID="Table2" runat="server" style="z-index: 1; left: 150px; top: 110px; position: absolute; height: 54px; width: 155px; right: 312px;">
+                <asp:TableRow>
+                    <asp:TableCell><asp:HyperLink ID="registro" runat="server" NavigateUrl="~/Registro.aspx">Quiero Regístrarme</asp:HyperLink></asp:TableCell></asp:TableRow><asp:TableRow>
+                    <asp:TableCell><asp:HyperLink ID="modificar" runat="server">Modificar Contraseña</asp:HyperLink></asp:TableCell></asp:TableRow></asp:Table><asp:Button ID="login" runat="server" Text="Login" style="top: 120px; left: 50px; position: absolute; height: 26px; width: 56px" /></div>
+    </form>
+</body>
+</html>
---
+++ labsHADS/labsHADS/Inicio.aspx.designer.cs
@@ -0,0 +1,96 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace labsHADS {
+    
+    
+    public partial class Inicio {
+        
+        /// <summary>
+        /// Control datos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table datos;
+        
+        /// <summary>
+        /// Control email.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox email;
+        
+        /// <summary>
+        /// Control erroremail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator erroremail;
+        
+        /// <summary>
+        /// Control password.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox password;
+        
+        /// <summary>
+        /// Control errorpass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator errorpass;
+        
+        /// <summary>
+        /// Control Table2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table2;
+        
+        /// <summary>
+        /// Control registro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink registro;
+        
+        /// <summary>
+        /// Control modificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink modificar;
+        
+        /// <summary>
+        /// Control login.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button login;
+    }
+}
